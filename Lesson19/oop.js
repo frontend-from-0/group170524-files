6eac5242c9ddfedf9ce4883194d989482f089679
Anna S. A <anna.atalay@minna.tech>
2024-10-08 23:15:43 +0300

    Add Lesson19 completed files

--- Lesson19/oop.js
+++ Lesson19/oop.js
@@ -9,3 +9,116 @@
   FP: problems are broken down into smaller, composable functions that can be combined to solve larger problems.
 
 */
+
+// const dog1 = {
+//   name: 'dog1',
+//   age: 5,
+//   breed: 'golden retriever',
+//   sound: function () {
+//     console.log('woof');
+//   },
+//   numberOfLegs: 4,
+// }; 
+
+// const dog2 = {
+//   name: 'dog2',
+//   age: 1,
+//   breed: 'poodle',
+//   sound: function () {
+//     console.log('woof');
+//   },
+//   numberOfLegs: 4,
+// };
+
+const duck = {
+  name: 'duck',
+  age: 3,
+  breed: 'mallard',
+  sound: function () {
+    console.log('quack');
+  },
+  numberOfLegs: 2,
+}
+
+const fish = {
+  name: 'fish',
+  age: 1,
+  breed: 'goldfish',
+}
+
+
+class Animal {
+  constructor(name, age, breed) {
+    // _ is used in mark properties as "private" inside a Class
+    this._name = name;
+    this._age = age;
+    this._breed = breed;
+  }
+
+  // get is a keyword that allows us to define a getter method
+  get name() {
+    return this._name.toUpperCase();
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  get breed() {
+    return this._breed;
+  }
+
+  // set is a keyword that allows us to define a setter method
+  set name(newName) {
+    this._name = newName.trim();
+  }
+
+  set age(newAge) {
+    if (newAge > 0) {
+      this._age = newAge;
+    } else {
+      console.error('Age must be greater than 0');
+    }
+  }
+
+  set breed(newBreed) {
+    this._breed = newBreed.trim();
+  }
+
+
+  // We don't use keyword "function" inside a class
+  describe() {
+    console.log('This is a ' + this._breed + ' named ' + this._name + ' that is ' + this._age + ' years old.');
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, breed) {
+    super(name, age, breed);
+    this._numberOfLegs = 4;
+  }
+
+  describe() {
+    console.log('This is a ' + this._breed + ' named ' + this._name + ' that is ' + this._age + ' years old with ' + this._numberOfLegs + ' legs.');
+  }
+
+  sound () {
+        console.log('woof');
+  }
+}
+
+const dog1 = new Animal('dog1', 5, 'golden retriever');
+const dog2 = new Dog('dog2', 1, 'poodle');
+
+console.log(dog1.name);
+console.log(dog1);
+
+dog1.name = 'Dog dog    ';
+dog1.age = -5;
+dog1.breed = 'Labrador';
+
+console.log(dog1);
+
+
+dog1.describe();
+dog2.describe();
--- Lesson19/script.js
+++ Lesson19/script.js
@@ -1,11 +1,76 @@
 // 1. Create a Person class with name, age, and gender properties, and a greet() method that logs a greeting message to the console.
 // Then create a Student class that extends the Person class and adds a major property. Override the greet() method in the Student class to log a different greeting message to the console. Create 2 variables, one of type Person and one of type Student and log them to console.
+class Person{
+    constructor(name,age,gender){
+        this._name=name;
+        this._age=age;
+        this._gender=gender;
+    }
+    greet(){
+        console.log('Hello my name is'+this._name+'I\'m '+this._age+' years old'+'and my gender is '+this._gender);
+    }
+}
+class Student extends Person{
+    constructor(name,age,gender,major){
+        super(name,age,gender);
+        this._major=major;
+    }
+    greet(){
+        console.log('Hello my name is'+this._name+'I\'m '+this._age+' years old'+'and my gender is '+this._gender+'And I\'m studying '+this._major);
+    }
+}
 
+const person1=new Person('Duygu',32,'Female');
+const person2=new Student('DuyguJr.',23,'Female','Computer Science');
 
+console.log(person1);
+console.log(person2);
+person2.greet();
 
 // 2. Create a Shape class with name and color properties, and a describe() method that logs a description of the shape to the console (e.g. "This is a blue square"). Then create a Square class that extends the Shape class and adds sideLength and area properties. Override the describe() method in the Square class to log a description of the square to the console (e.g. "This is a blue square with a side length of 5 and an area of 25").
 
-// 3. Create a Vehicle class with make, model, and year properties, and a start() method that logs a message to the console indicating that the vehicle has started. Then create a Car class that extends the Vehicle class and adds a numWheels property. Override the start() method in the Car class to log a different message to the console indicating that the car has started.
+class Shape{
+    constructor(name,color) {
+      this._name=name;
+      this._color=color;
+    }
+
+    describe(){
+      console.log('This is a '+this._color+' '+this._name);
+  }
+}
+
+class Square extends Shape{
+    constructor(color,sideLength){
+      super('square',color);
+      this._sideLength=sideLength;
+      this._area=sideLength * sideLength;
+    }
+
+    describe(){
+      console.log(`This is a ${this._color} square with a side length  of ${this._sideLength} and an area of ${this._area}`);
+    }
+
+    describeShape () {
+      return super.describe();
+    }
+}
+
+
+
+const square1=new Shape('Square','blue');
+square1.describe();
+console.log (square1);
+
+const square2=new Square('red',5);
+square2.describe();
+square2.describeShape();
+
+
+
+
+// 3. Create a Vehicle class with make, model, and year properties, and a start() method
+// that logs a message to the console indicating that the vehicle has started. Then create a Car class that extends the Vehicle class and adds a numWheels property. Override the start() method in the Car class to log a different message to the console indicating that the car has started.
 
 
 // 4. Create a BankAccount class with balance and interestRate properties, and deposit() and withdraw() methods. Add a transactionHistory property that is an array of objects representing each transaction made on the account, with properties for type (either "deposit" or "withdrawal"), amount, and date ({type: "withdrawal", amount: amount, date: new Date().toLocaleDateString()}). Implement deposit() and withdraw() methods that add new transactions to the transactionHistory array. Implement a get transactionHistory() getter method that returns a copy of the transaction history array, and a get currentBalance() getter method that calculates and returns the current balance of the account based on the transaction history.
