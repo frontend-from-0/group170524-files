98eb8c6bd71f5caa1c262e4420cdbd15dda29720
Anna S. A <anna.atalay@minna.tech>
2025-01-14 20:01:23 +0100

    Add Lesson31 completed files

--- Lessson31/random-quotes/src/App.js
+++ Lessson31/random-quotes/src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import "./App.css";
+import { quotes as quotesData} from "./quotes";
+import { useState } from "react";
+import { QuoteBox } from "./components/QuoteBox";
+import { Title } from "./components/Title";
 
 function App() {
+  const [quotes, setQuotes] = useState(quotesData);
+  const [quoteIndex, setQuoteIndex] = useState(0);
+  // const [likeCount, setLikeCount] = useState(0);
+  const getRandomQuoteIndex = () => Math.floor(Math.random() * quotes.length);
+
+  function handleNewQuoteClick() {
+    setQuoteIndex(getRandomQuoteIndex());
+  }
+
   return (
     <div className="App">
-      <h1>Random Quotes</h1>
+      <Title>Random Quotes</Title>
+      <QuoteBox
+        quote={quotes[quoteIndex].quote}
+        author={quotes[quoteIndex].author}
+        onNewQuoteClick={handleNewQuoteClick}
+      />
     </div>
   );
 }
